38f1fb6c840001168af3bcb29238175513d1ae10
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-30 13:57:33 -0300

    Add files via upload

---
+++ questao1.11.py
@@ -0,0 +1,16 @@
+# Atribuição de variáveis
+
+# Entrada de dados
+
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+d = float(input("d: "))
+
+#Processamento de dados
+
+mean = (a+b+c+d)/4
+
+# Saída de dados
+
+print("A média dos números fornecidos é: " + str(mean))
